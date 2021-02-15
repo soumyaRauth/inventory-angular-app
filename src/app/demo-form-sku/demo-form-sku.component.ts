8a18ea1b1ca2b320fb364d8d974e1c3bf23ed96e
soumyaRauth <shoummorauth@live.co.uk>
2021-02-16 02:17:22 +0600

    custom reactive form validation added

--- src/app/demo-form-sku/demo-form-sku.component.ts
+++ src/app/demo-form-sku/demo-form-sku.component.ts
@@ -2,26 +2,71 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormControl,AbstractControl, FormGroup, Validators } from '@angular/forms';
 
 
+
+// function skuValidator(control: FormControl): { [s: string]: boolean } {
+//   if (!control.value.match(/^123/)) {
+//     return {invalidSku: true};
+//   }
+// }
+
+
 @Component({
   selector: 'demo-form-sku',
   templateUrl: './demo-form-sku.component.html',
   styleUrls: ['./demo-form-sku.component.css']
 })
 export class DemoFormSKUComponent implements OnInit {
-  myForm: FormGroup;
-  sku:FormControl=new FormControl()
-  constructor(fb: FormBuilder) {
-    this.myForm = fb.group({
-      'sku': ['',Validators.required]
-    });
+myForm:FormGroup;
+sku:FormControl=new FormControl();
+
+constructor(private fb:FormBuilder)
+{
+  this.myForm=fb.group({
+    'sku':['', Validators.compose([Validators.required,skuContainsNum,skuStartsWithNum]) ]
+  })
+
+ this.myForm.valueChanges.subscribe((form:FormGroup)=>{
+   console.log(form['sku']);
+   
+ })
+  
+}
 
 
-  }
+
+
+  
 
   ngOnInit() {
   }
 
   onSubmit(value: string): void {
     console.log('you submitted value: ', value);
+  }
+}
+
+
+function skuStartsWithNum(control: AbstractControl): {[key:string]:any} | null{
+
+  if(control.value.match(/^\d/)){
+    return {'startsWithNum':true}
+  }else{
+    return null;
+  }
+  
+}
+
+
+/**
+  * Contains Number Validator
+  */
+ function skuContainsNum(control:AbstractControl):{[key:string]:any}|null{
+   console.log("SKU Contains num");
+   console.log(/\d/.test(control.value));
+   
+  if(/\d/.test(control.value)){
+    return null;
+  }else{
+    return {'skuContainsNumber':true}
   }
 }
